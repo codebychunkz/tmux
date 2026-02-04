dd7bb6cc422129256cb45db8fb502604f19e454c
David Lidzén <lidz.dav@gmail.com>
2026-02-04 10:12:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 # REQUIRED
-sudo dnf install zsh alacritty tmux
+sudo dnf install zsh alacritty tmux git
+
+#TMUX PACKAGE MANAGER
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #SETUP ATUIN
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
